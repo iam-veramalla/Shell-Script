632bcdc7df7e4c74792195a46e7853441ea437d7
iam-veramalla <ramya.bikki1990@gmail.com>
2024-09-20 13:43:50 +0100

    10thCommit

---
+++ 10-Functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+ if [ $? -ne 0 ]
+then
+    echo " installation is failure"
+    exit 1
+else
+    echo " Installation Success"
+fi    
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run the script with root access"
+    exit 1
+else
+    echo "You are Root user"
+fi  
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
+
